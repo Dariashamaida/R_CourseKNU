4b0ae1a808695f97db8685581751b5f5b08fcc74
Dariashamaida <61041300+Dariashamaida@users.noreply.github.com>
2020-06-09 23:33:51 +0300

    Update pmean.R

--- pmean.R
+++ pmean.R
@@ -1,1 +1,19 @@
+# In this file codes for functions are saved
 
+### Setting work directory
+
+setwd("G:\\KNU\\5 курс\\2 семестр\\R\\rprog_data_specdata")
+
+### pmean function
+
+> pmean <- function(directory, pollutant, id=1:length(file_list)){
++    file_list <-  dir(path=directory, pattern =".csv", full.names=TRUE)
++     x <- numeric()
++     for (i in id){
++         all_data <- read.csv(file_list[i])
++         x <- c(x, all_data[[pollutant]])
++     }
++     mean(x, na.rm=TRUE)
++ }
+> 
+> pmean("specdata", "sulfate", 1:10)
